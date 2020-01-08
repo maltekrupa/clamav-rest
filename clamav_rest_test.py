68acc67d287173891e65d7732d4f49a433a5f8c6
Malte Krupa <github-nqyroppg@nafn.de>
2020-01-08 08:26:24 +0100

    s/"/'/g #patch

--- clamav_rest_test.py
+++ clamav_rest_test.py
@@ -13,47 +13,47 @@ class ClamAVRESTTestCase(unittest.TestCase):
         self.app = clamav_rest.app.test_client()
 
     def test_healthcheck_live(self):
-        response = self.app.get("/health/live")
+        response = self.app.get('/health/live')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'OK')
 
-    @patch("clamav_rest.cd.ping")
+    @patch('clamav_rest.cd.ping')
     def test_healthcheck_ready(self, ping):
         ping.return_value = 'PONG'
 
-        response = self.app.get("/health/ready")
+        response = self.app.get('/health/ready')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Service OK')
 
-    @patch("clamav_rest.cd.ping")
+    @patch('clamav_rest.cd.ping')
     def test_healthcheck_no_service(self, ping):
         ping.side_effect = clamd.ConnectionError()
 
-        response = self.app.get("/health/ready")
+        response = self.app.get('/health/ready')
 
         self.assertEqual(response.status_code, 502)
         self.assertEqual(response.data, b'Service Unavailable')
 
-    @patch("clamav_rest.cd.ping")
+    @patch('clamav_rest.cd.ping')
     def test_healthcheck_unexpected_error(self, ping):
-        ping.side_effect = Exception("Oops")
+        ping.side_effect = Exception('Oops')
 
-        response = self.app.get("/health/ready")
+        response = self.app.get('/health/ready')
 
         self.assertEqual(response.status_code, 500)
         self.assertEqual(response.data, b'Service Unavailable')
 
-    @patch("clamav_rest.cd.ping")
+    @patch('clamav_rest.cd.ping')
     def test_healthcheck_unexpected_error_original(self, ping):
-        ping.side_effect = Exception("Oops")
+        ping.side_effect = Exception('Oops')
 
-        response = self.app.get("/health/ready")
+        response = self.app.get('/health/ready')
 
         self.assertEqual(response.status_code, 500)
         self.assertEqual(response.data, b'Service Unavailable')
 
     def test_scan_endpoint_requires_post(self):
-        response = self.app.get("/")
+        response = self.app.get('/')
 
         self.assertEqual(response.status_code, 405)
 
