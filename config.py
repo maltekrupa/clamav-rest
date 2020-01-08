a83c6424367c5eb7bde046dbc8b58cb98fef8ee1
Malte Krupa <github-nqyroppg@nafn.de>
2020-01-08 14:15:34 +0100

    Remove ability to run flask without gunicorn #patch

--- config.py
+++ config.py
@@ -3,5 +3,3 @@ import os
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO')
 CLAMD_HOST = os.environ.get('CLAMD_HOST', 'localhost')
 CLAMD_PORT = int(os.environ.get('CLAMD_PORT', 3310))
-LISTEN_HOST = os.environ.get('HOST', '0.0.0.0')
-LISTEN_PORT = int(os.environ.get('PORT', 8080))
