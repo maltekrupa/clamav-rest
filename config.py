0f36e2844fb36933294936f58d2b52f8918d4e88
Malte Krupa <github-nqyroppg@nafn.de>
2020-02-19 08:11:33 +0100

    Add optional basic auth. Closes #2

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+from werkzeug.security import generate_password_hash
 import os
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO')
 CLAMD_HOST = os.environ.get('CLAMD_HOST', 'clamav')
 CLAMD_PORT = int(os.environ.get('CLAMD_PORT', 3310))
+
+AUTH_USERNAME = os.environ.get('AUTH_USERNAME', None)
+AUTH_PASSWORD = os.environ.get('AUTH_PASSWORD', None)
+if AUTH_PASSWORD:
+    AUTH_PASSWORD_HASH = generate_password_hash(AUTH_PASSWORD)
+else:
+    AUTH_PASSWORD_HASH = None
