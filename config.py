2ac412a30cd169b970144d8b43587b2394e762f3
Doug Hoke <douglass.hoke@gmail.com>
2023-07-06 03:21:40 -0400

    Add limits (#42)
    * initial changes - testing still required
    
    * Update Dockerfile to match version 16
    
    ---------
    
    Co-authored-by: Malte <github-nqyroppg@nafn.de>

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ AUTH_USERNAME = os.environ.get('AUTH_USERNAME', None)
 AUTH_PASSWORD = os.environ.get('AUTH_PASSWORD', None)
 
 MAX_CONTENT_LENGTH = int(os.environ.get('MAX_CONTENT_LENGTH', 16777216))
+RESPONSE_TIMEOUT = int(os.environ.get('RESPONSE_TIMEOUT', 60))
+BODY_TIMEOUT = int(os.environ.get('BODY_TIMEOUT', 60))
